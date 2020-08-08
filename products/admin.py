abaa324a74798c082a0aec55eab69f2cf8d6c8ae
TheoNorris <theonorris@live.com>
2020-08-08 10:05:24 +0000

    Customised admin

--- products/admin.py
+++ products/admin.py
@@ -2,5 +2,27 @@ from django.contrib import admin
 from .models import Category, Product
 
 
-admin.site.register(Category)
-admin.site.register(Product)
+class CategoryAdmin(admin.ModelAdmin):
+
+    list_display = (
+        'name',
+    )
+
+
+class ProductAdmin(admin.ModelAdmin):
+
+    list_display = (
+        'category',
+        'sku',
+        'name',
+        'description',
+        'flavours',
+        'price',
+        'rating',
+    )
+
+    ordering = ('sku',)
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Product, ProductAdmin)
--- products/models.py
+++ products/models.py
@@ -2,14 +2,14 @@ from django.db import models
 
 
 class Category(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
     name = models.CharField(max_length=100)
-    friendly_name = models.CharField(max_length=100, null=True, blank=True),
 
     def __str__(self):
         return self.name
-
-    def get_friendly_name(self):
-        return self.friendly_name
 
 
 class Product(models.Model):
@@ -18,6 +18,7 @@ class Product(models.Model):
     sku = models.CharField(max_length=100, null=True, blank=True)
     name = models.CharField(max_length=100)
     description = models.TextField()
+    flavours = models.BooleanField(default=False, null=True, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     rating = models.DecimalField(max_digits=2, decimal_places=0, null=True,
                                  blank=True)
