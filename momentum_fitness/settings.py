0ab8afd48ea16715ae6450b79b3ddbc1df365963
TheoNorris <theonorris@live.com>
2020-08-11 14:12:04 +0000

    Added cart context

--- momentum_fitness/settings.py
+++ momentum_fitness/settings.py
@@ -106,6 +106,7 @@ TEMPLATES = [
                 'django.template.context_processors.request', # required by allauth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'cart.contexts.cart_contents',
             ],
         },
     },
@@ -188,3 +189,6 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+FREE_DELIVERY_THRESHOLD = 20
+STANDARD_DELIVERY_PERCENTAGE = 10
