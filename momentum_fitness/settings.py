9651ea2695df565f3493e6fa0151e9c5cacc45ba
TheoNorris <theonorris@live.com>
2020-08-13 11:59:15 +0000

    Added basic toasts to functions

--- momentum_fitness/settings.py
+++ momentum_fitness/settings.py
@@ -112,6 +112,8 @@ TEMPLATES = [
     },
 ]
 
+MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
+
 AUTHENTICATION_BACKENDS = [
 
     # Needed to login by username in Django admin, regardless of `allauth`
