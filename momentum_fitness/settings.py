b84e4c6eaac0d4aa9bda7d1af2aa2748686518df
TheoNorris <theonorris@live.com>
2020-08-21 15:36:10 +0000

    Added print statement to secret key

--- momentum_fitness/settings.py
+++ momentum_fitness/settings.py
@@ -26,6 +26,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
+print('Key is: ', SECRET_KEY)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
