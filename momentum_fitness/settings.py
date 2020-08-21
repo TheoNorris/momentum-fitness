19bc459cdf62ccb76a7097400cd63dacde874800
TheoNorris <theonorris@live.com>
2020-08-21 18:27:24 +0000

    heroku testing

--- momentum_fitness/settings.py
+++ momentum_fitness/settings.py
@@ -127,13 +127,17 @@ WSGI_APPLICATION = 'momentum_fitness.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 
