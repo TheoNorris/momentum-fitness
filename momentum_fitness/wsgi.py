fa2d7dfed7915ded71b518d2c376ca120137de94
TheoNorris <theonorris@live.com>
2020-08-21 19:16:56 +0000

    Attempted heroku deployment correction

--- momentum_fitness/wsgi.py
+++ momentum_fitness/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'momentum_fitness')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'momentum_fitness.settings')
 
 application = get_wsgi_application()
